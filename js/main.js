adfbbd79b4921093459cedab29a05e5926ac4e3a
Mariano Timistit <mariano@wcanvas.com>
2021-07-19 20:14:11 -0300

    Page Update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,81 @@
-console.time('JS is loading fine')
 
+(function () {
+    window.addEventListener('load', () => {
 
-/* Navbar Active
-document.addEventListener('scroll', function() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById('js-nav').classList.add('active')
-    } else {
-        document.getElementById('js-nav').classList.remove('active')
-    }
-})*/
+        console.time('JS is loading fine')
 
+        // Variables
+        const menuItemProfesionales = document.getElementById('js-profesionales')
+        const menuItemAreasPractica = document.getElementById('js-areas-practica')
+        const menuItemContacto = document.getElementById('js-contacto')
+
+        // Barba
+        barba.init({
+            transitions: [{
+                sync: false, // sync the transitions up so they occur concurrently
+                leave({current}) {
+                    const targets = current.container; // target current page container
+                    const a = anime({ 
+                      targets: targets, 
+                      opacity: [1, 0], 
+                      duration: 400, 
+                      easing: 'linear', 
+                    });
+                    return a.finished;
+                },
+                after({next}) {
+                    const targets = next.container; // target next page container
+                    const a = anime({ 
+                      targets: targets, 
+                      opacity: [0, 1], 
+                      duration: 400, 
+                      easing: 'linear', 
+                    });
+                    return a.finished;
+                }
+            }],
+            views: [{
+                namespace: 'inicio',
+                beforeEnter(data) {
+                    document.querySelector('nav').classList.remove('active')
+                },
+                beforeLeave(data) {
+                    document.querySelector('nav').classList.add('active')
+                }
+            }, {
+                namespace: 'profesionales',
+                beforeEnter(data) {
+                    menuItemProfesionales.classList.add('active')
+                },
+                beforeLeave(data) {
+                    menuItemProfesionales.classList.remove('active')
+                }
+            }, 
+            {
+                namespace: 'areas-practica',
+                beforeEnter(data) {
+                    menuItemAreasPractica.classList.add('active')
+                },
+                beforeLeave(data) {
+                    menuItemAreasPractica.classList.remove('active')
+                }
+            },
+            {
+                namespace: 'contacto',
+                beforeEnter(data) {
+                    menuItemContacto.classList.add('active')
+                },
+                beforeLeave(data) {
+                    menuItemContacto.classList.remove('active')
+                }
+            }]
+        });
+
+        console.timeEnd("JS is loading fine");
+
+    });
+
+})();
 
 /* Hamburger Menu
 const hamburgerMenuContainer = document.getElementById('js-hambuerger-menu-container')
@@ -26,25 +92,6 @@ hamburgerMenuContainer.addEventListener('click', () => {
     }
 })*/
 
-// Barba
-barba.init({
-    transitions: [{
-      name: 'opacity-transition',
-      leave() {
-        anime({
-            targets: '.anime-js',
-            translateX: 1000
-          });
-      },
-      enter() {
-        anime({
-            targets: '.anime-js',
-            translateX: 1000
-          });
-      }
-    }]
-  });
+// Menu Active Items
 
-
-console.timeEnd("JS is loading fine");
 
